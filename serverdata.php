12b438d8133dfdce84eb8056172f6839fabf6b73
MoonWave1 <154413975+MoonWave1@users.noreply.github.com>
2024-08-09 04:47:08 +0300

    Create serverdata.php

---
+++ serverdata.php
@@ -0,0 +1,1 @@
+server|127.0.0.1\nport|17191\nloginurl|login.growtopiagame.com\ntype|1\n#maint|Under mainteance.\nbeta_server|127.0.0.1\nbeta_port|1945\nbeta_type|1\nmeta|defined\nRTENDMARKERBS1001\nunknown
